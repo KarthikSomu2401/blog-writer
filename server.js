1207e9a32c9727271932e49ac92967b996269f7d
Karthikeyan Somasundaram <karthikitmsp@gmail.com>
2020-04-03 13:38:41 +0100

    Depersonalisation Commit

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const envs = require('./configurations');
 var app = require('express')();
 
-console.log(`Your environment is ${envs.NODE_ENV}`);
-console.log(`Your port is ${envs.PORT}`);
-console.log(`Your url is ${envs.MONGO_URL}`);
-
 var listener = app.listen(envs.PORT, function(){
     console.log('Listening on port ' + listener.address().port);
 });

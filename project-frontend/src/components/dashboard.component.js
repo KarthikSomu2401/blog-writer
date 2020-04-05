42bd2b884398e868a366fa7b7191393b28fdd0c9
Karthikeyan Somasundaram <karthikitmsp@gmail.com>
2020-04-05 15:44:16 +0100

    minor changes

--- project-frontend/src/components/dashboard.component.js
+++ project-frontend/src/components/dashboard.component.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-//import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import { Navbar, NavbarBrand, NavItem, Nav, NavLink } from "react-bootstrap";
+import { Navbar, NavbarBrand, Nav, NavLink } from "react-bootstrap";
 
 export default class Dashboard extends Component {
   render() {
@@ -9,9 +8,9 @@ export default class Dashboard extends Component {
         <NavbarBrand>WebSiteName</NavbarBrand>
         <Nav>
           <NavLink>Home</NavLink>
-          <NavLink>Page 1</NavLink>
-          <NavLink>Page 2</NavLink>
-          <NavLink>Page 3</NavLink>
+          <NavLink>About</NavLink>
+          <NavLink>Accout</NavLink>
+          <NavLink>Logout</NavLink>
         </Nav>
       </Navbar>
     );
